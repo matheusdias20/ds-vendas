9cfb73a404d11060936014717247cb068bf7ca05
Matheus Vieira <mvdias20@gmail.com>
2021-05-07 09:57:00 -0300

    Home, navigation

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
+import NavBar from "components/NavBar";
 import BarChart from "components/BarChart";
 import DonutChart from "components/DonutChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
-import NavBar from "components/NavBar";
 
 
 
