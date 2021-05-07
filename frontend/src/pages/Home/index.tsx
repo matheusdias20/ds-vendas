52c68eab0e791b532a8bd24acfa6c820670b6a82
Matheus Vieira <mvdias20@gmail.com>
2021-05-07 09:50:34 -0300

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
+const  Home = () => {
+    return (
+        <>
+            <NavBar />
+            <Footer />
+        </>
+    );
+}
+  
+export default Home;
